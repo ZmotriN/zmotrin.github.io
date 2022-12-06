9d9108d814eff5cb04597ee228fb846a33da3fe2
ZmotriN <zmotrin@gmail.com>
2022-12-06 10:24:30 -0500

    Mockup9

--- cours/shared/jscripts/article.js
+++ cours/shared/jscripts/article.js
@@ -1,3 +1,4 @@
+
 const app = Vue.createApp({
     data() {
         return {
@@ -16,16 +17,35 @@ const app = Vue.createApp({
 
 
 
-app.component('grostitre', {
-    props: ['name', 'id'],
+app.component('tabledesmatieres', {
     data() {
         return {
+            list: '',
         }
     },
     created() {
+        this.$nextTick(() => {
+            let lis = '';
+            this.$root.tableOfContents.forEach(el => { lis += '<li><a href="#' + el.id + '">' + el.name + '</a></li>'; });
+            this.list = lis;
+        });
+    },
+    template: `
+        <div id="contents_table">
+            <div class="contents_table__table">
+                <a href="#top"><strong>Table des matières</strong></a>
+                <ul v-html="list"></ul>
+            </div>
+        </div>`
+});
+
+
+
+app.component('grostitre', {
+    props: ['name', 'id'],
+    created() {
         this.$root.addToTableOfContents(this.id, this.name)
     },
-
     methods: {
         click(event) {
             var link = window.location.origin + window.location.pathname + '#' + this.id;
@@ -43,36 +63,9 @@ app.component('grostitre', {
             <h1>{{ this.name }}</h1>
             <div class="chain" @click="click($event)"><svg viewBox="0 0 24 24"><path d="M17 7h-4v2h4c1.65 0 3 1.35 3 3s-1.35 3-3 3h-4v2h4c2.76 0 5-2.24 5-5s-2.24-5-5-5zm-6 8H7c-1.65 0-3-1.35-3-3s1.35-3 3-3h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-2zm-3-4h8v2H8z"></path></svg></div>
             <div class="linkcopied">Lien copié &#x2713;</div>
-        </div>
-    `
+        </div>`
 });
 
-
-app.component('tabledesmatieres', {
-    data() {
-        return {
-            list: '',
-        }
-    },
-    created() {
-        this.$nextTick(() => {
-            let lis = '';
-            this.$root.tableOfContents.forEach(el => { lis += '<li><a href="#' + el.id + '">' + el.name + '</a></li>'; });
-            this.list = lis;
-        });
-    },
-    methods: {
-
-    },
-    template: `
-        <div id="contents_table">
-            <div class="contents_table__table">
-                <a href="#top"><strong>Table des matières</strong></a>
-                <ul v-html="list"></ul>
-            </div>
-        </div>
-    `
-});
 
 
 
@@ -81,25 +74,71 @@ app.component('codepen', {
     data() {
         return {
             user: 'ZmotriN',
-            theme: 'light',
-            // theme: '39618',
+            // theme: 'light',
+            theme: '39618',
         }
     },
     template: `
         <iframe
+            :src="'https://codepen.io/' + user + '/embed/' + id + '?default-tab=html%2Cresult&theme-id=' + theme"    
             class="codepen"
-            title="Flex-direction"
             scrolling="no"
-            :src="'https://codepen.io/' + user + '/embed/' + id + '?default-tab=html%2Cresult&theme-id=' + theme"
             frameborder="no"
             loading="lazy"
             allowtransparency="true"
             allowfullscreen="true"
-        ></iframe>
-    `
+        ></iframe>`
 });
 
 
+app.component('exercice', {
+    props: ['id'],
+    data() {
+        let exroot = '/cours/shared/exercices/' + this.id + '/';
+        let exjson = exroot + 'details.json';
+        let exdetails = syncjson(exjson);
+        let url = typeof exdetails.url == 'undefined' ? exroot : exdetails.url;
+        return {
+            name: exdetails.name,
+            description: exdetails.description,
+            url: url
+        }
+    },
+    template: `
+        <a class="exercice" target="_blank" :href="this.url">
+            <div class="exercice-container">
+                <div class="exercice-thumb" :style="'background-image: url(\\'/cours/shared/exercices/' + this.id + '/thumb.jpg\\')'"></div>
+                <div class="exercice-abstract">
+                    <em>EXERCICE</em><br>
+                    <span class="exercice-title">{{ name }}</span><br>
+                    <span class="exercice-description">{{ description }}</span>
+                </div>
+            </div>
+        </a>`
+});
+
+
+
+app.component('doclink', {
+    props: ['title', 'href'],
+    data() {
+        let site = '';
+        let url = new URL(this.href);
+        // console.log(url.hostname);
+        switch(url.hostname) {
+            case 'www.w3schools.com': site = 'w3schools'; break;
+            case 'developer.mozilla.org': site = 'mozilla'; break;
+        }
+        return { class: site }
+    },
+    template: `
+        <a :class="'doclink ' + this.class" target="_blank" :href="this.href">
+            <div class="doclink-container">
+                <div class="doclink-icon"></div>
+                <span class="doclink-title">{{ title }}</span>
+            </div>
+        </a>`
+});
 
 
 
