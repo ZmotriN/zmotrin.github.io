440b2a40ef63e270d84b83b8b1401095adbd0fd3
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-05-21 00:35:42 -0400

    Exercices Part 1

--- exercices-css/_index.php
+++ exercices-css/_index.php
@@ -6,7 +6,16 @@
  * @abstract Collection d'exercices CSS3
  */
 ?>
+
+<exercice href="./air-premiers-symptomes/"></exercice>
+<exercice href="./asian-dub-foundation-rafis-revenge/"></exercice>
+<exercice href="./eagles-of-death-metal-peace-love-death-metal/"></exercice>
 <exercice href="./godspeed-you-black-emperor-lift-yr/"></exercice>
+<exercice href="./king-crimson-larks-tongues-in-aspic/"></exercice>
+<exercice href="./lost-highway/"></exercice>
 <exercice href="./mr-oizo-all-wet/"></exercice>
+<exercice href="./nirvana-nevermind/"></exercice>
+<exercice href="./the-black-keys-turn-blue/"></exercice>
 <exercice href="./trainspotting/"></exercice>
 <exercice href="./ttc-3615/"></exercice>
+<exercice href="./underworld-everything-everything/"></exercice>
