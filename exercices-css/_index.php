1ea6b0779d2e719c10ebeed3bf075f30ea57a8fb
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-05-21 08:49:32 -0400

    Exercices Part 2

--- exercices-css/_index.php
+++ exercices-css/_index.php
@@ -11,10 +11,15 @@
 <exercice href="./asian-dub-foundation-rafis-revenge/"></exercice>
 <exercice href="./eagles-of-death-metal-peace-love-death-metal/"></exercice>
 <exercice href="./godspeed-you-black-emperor-lift-yr/"></exercice>
+<exercice href="./harmonium-l-heptade/"></exercice>
 <exercice href="./king-crimson-larks-tongues-in-aspic/"></exercice>
+<exercice href="./les-cowboys-fringants-break-syndical/"></exercice>
+<exercice href="./lisa-leblanc-lisa-leblanc/"></exercice>
 <exercice href="./lost-highway/"></exercice>
 <exercice href="./mr-oizo-all-wet/"></exercice>
 <exercice href="./nirvana-nevermind/"></exercice>
+<exercice href="./pink-floyd-the-dark-side-of-the-moon/"></exercice>
+<exercice href="./robert-charlebois-la-maudite-tournee/"></exercice>
 <exercice href="./the-black-keys-turn-blue/"></exercice>
 <exercice href="./trainspotting/"></exercice>
 <exercice href="./ttc-3615/"></exercice>
