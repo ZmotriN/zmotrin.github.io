3e74ee6f6dcae662ca188697d040c062a99a273a
Maxime Larrivée-Roy <zmotrin@gmail.com>
2025-01-31 20:35:57 -0500

    Carte test v001

---
+++ carte/jscripts/main.js
@@ -0,0 +1,12 @@
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user they can install the PWA
+    showInstallPromotion();
+    // Optionally, send analytics event that PWA install promo was shown.
+    console.log(`'beforeinstallprompt' event was fired.`);
+    alert(`'beforeinstallprompt' event was fired.`);
+  });
+  console.log("test");
--- carte/worker.js
+++ carte/worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `maximelroy-carte-v1`;
+const CACHE_NAME = `maximelroy-carte-v2`;
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
