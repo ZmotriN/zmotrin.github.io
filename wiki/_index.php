c824e5a274cb68d7a065ac47be0e340e12832ff1
Maxime Larrivée-Roy <zmotrin@gmail.com>
2025-02-19 21:23:32 -0500

    Gossage de couleurs + Wiki

---
+++ wiki/_index.php
@@ -0,0 +1,21 @@
+<?php 
+/**
+ * @type     wiki
+ * @title    Wiki - Intégration Web
+ * @_icon     images/icon.png
+ * @abstract Intégration Web
+ */
+?>
+
+<wiki-page name="Cours 01A">
+asdf
+</wiki-page>
+
+
+<wiki-page name="Cours 01B">
+asdf
+</wiki-page>
+
+
+<wiki-page name="Cours 02A">
+</wiki-page>
