438c43d5ad7b23e51db3af5af841dee2898619ae
Maxime Larrivée-Roy <zmotrin@gmail.com>
2025-03-01 00:49:28 -0500

    Beaucoup de crawler

--- TODO.md
+++ TODO.md
@@ -21,3 +21,9 @@
 
 
 - git clone --recurse-submodules https://github.com/ZmotriN/zmotrin.github.io.git
+
+
+
+    <script
+        src="data:application/x-javascript; charset=utf-8;base64,Oy8qRkJfUEtHX0RFTElNKi8KCnZhciBkYXRhRWxlbWVudD1kb2N1bWVudC5nZXRFbGVtZW50QnlJZCgiZW52anNvbiIpO2lmKGRhdGFFbGVtZW50IT1udWxsKXt2YXIgY29weVZhcmlhYmxlcz1mdW5jdGlvbihhKXtmb3IodmFyIGIgaW4gdmFyaWFibGVzKWFbYl09dmFyaWFibGVzW2JdfSx2YXJpYWJsZXM9SlNPTi5wYXJzZShkYXRhRWxlbWVudC50ZXh0Q29udGVudCk7d2luZG93LnJlcXVpcmVMYXp5P3dpbmRvdy5yZXF1aXJlTGF6eShbIkVudiJdLGNvcHlWYXJpYWJsZXMpOih3aW5kb3cuRW52PXdpbmRvdy5FbnZ8fHt9LGNvcHlWYXJpYWJsZXMod2luZG93LkVudikpfQovLyMgc291cmNlVVJMPWh0dHBzOi8vc3RhdGljLnh4LmZiY2RuLm5ldC9yc3JjLnBocC92NC95TC9yL2RiZ3JNc25EMThwLmpzCg=="
+        data-bootloader-hash="nxRKQO6" crossorigin="anonymous" data-p=":0" data-c="1"></script>
--- manuel/test/_index.php
+++ manuel/test/_index.php
@@ -3,35 +3,29 @@
  * @type     article
  * @title    Test
  * @icon     images/icon.webp
- * @image    ../images/image.webp
+ * @image    ../images/image.webp    
  * @abstract Test
  */
 ?>
 
+<boxlink href="https://www.comediegeek.com/humour/humoriste/humoriste-katherine-levac-partage-de-rares-photos-de-ses-jumeaux"></boxlink>
 
-<!-- <error title="FFMPEG Error">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a urna non diam ornare dictum nec at lorem. Fusce venenatis magna vitae mauris euismod varius.</error> -->
+<boxlink href="https://deadline.com/2025/01/best-screenplay-oscar-nominees-2025-read-1236264823/"></boxlink>
+
+<boxlink href="https://www.arte.tv/en/videos/102302-000-A/walden-life-in-the-woods/"></boxlink>
+
+<boxlink href="https://interestingengineering.com/science/first-ever-multiplex-quantum-entanglement-us"></boxlink>
+
+<boxlink href="https://www.nasa.gov/news-release/liftoff-nasa-tech-science-en-route-to-moon-with-intuitive-machines/"></boxlink>
+
+<boxlink href="https://snyder.substack.com/p/five-failures-in-the-oval-office"></boxlink>
+
+<boxlink href="https://www.nasa.gov/learning-resources/internship-programs/"></boxlink>
+
+<boxlink href="https://variety.com/2025/film/news/rob-lowe-sex-scenes-brave-nicole-kidman-babygirl-1236323922/"></boxlink>
+
+<boxlink href="https://loudwire.com/slayer-2025-canadian-feq-festival/"></boxlink>
 
 
-<boxlink href="https://ici.radio-canada.ca/nouvelle/2143518/ukraine-entente-minerais-critiques-etats-unis"></boxlink>
-
-<boxlink href="https://www.tvanouvelles.ca/2025/02/25/lacces-a-la-presse-renforce-par-la-maison-blanche-un-leader-dans-une-democratie-ne-devrait-pas-choisir-les-journalistes"></boxlink>
-
-<boxlink href="https://www.soundjay.com/fart-sound-effect.html"></boxlink>
-
-<boxlink href="https://developer.mozilla.org/en-US/docs/Web/CSS/flex-direction"></boxlink>
-
-<boxlink href="https://www.indiewire.com/news/general-news/steve-buscemi-robert-pattinson-mickey-17-fargo-accent-1235098814/?link_source=ta_first_comment&taid=67be4b94e352d90001eb51a3"></boxlink>
-
-<boxlink href="https://www.nme.com/news/tv/chris-odowd-tells-press-discuss-trans-issues-with-graham-linehan-instead-sowing-division-3841290"></boxlink>
-
-<boxlink href="https://www.ledevoir.com/opinion/chroniques/847972/chronique-fatigue-colosse"></boxlink>
-
-<boxlink href="https://www.lesacdechips.com/2025/02/25/jai-ecoute-la-chanson-rnb-du-fils-de-justin-trudeau-et-voici-mes-observations"></boxlink>
-
-<boxlink href="https://www.rds.ca/hockey/m18-aaa/m18-aaa-saison-de-reve-pour-les-chevaliers-de-levis-1.19870511"></boxlink>
-
-<boxlink href="https://vedettequebec.com/2025/02/25/apres-rupture-daniel-corbin-annonce-nouvelle-blonde/"></boxlink>
-
-<boxlink href="https://www.stereogum.com/2298178/kate-bush-new-order-damon-albarn-among-1000-uk-artists-protesting-governments-ai-stance-with-a-silent-album/news/"></boxlink>
 
 <!-- boxlink href="../../exercices-css/#end"></boxlink -->
