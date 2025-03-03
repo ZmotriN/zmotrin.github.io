6cbe344578c33024685874142eb854a8d4d17575
Maxime Larrivée-Roy <zmotrin@gmail.com>
2025-03-03 01:10:43 -0500

    Scraping encore

--- manuel/test/_index.php
+++ manuel/test/_index.php
@@ -8,24 +8,11 @@
  */
 ?>
 
-<boxlink href="https://www.comediegeek.com/humour/humoriste/humoriste-katherine-levac-partage-de-rares-photos-de-ses-jumeaux"></boxlink>
+<boxlink href="https://bmjgroup.com/back-pain-recommendations/"></boxlink>
 
-<boxlink href="https://deadline.com/2025/01/best-screenplay-oscar-nominees-2025-read-1236264823/"></boxlink>
+<boxlink href="https://www.guinnessworldrecords.com/world-records/largest-speeding-fine"></boxlink>
 
-<boxlink href="https://www.arte.tv/en/videos/102302-000-A/walden-life-in-the-woods/"></boxlink>
-
-<boxlink href="https://interestingengineering.com/science/first-ever-multiplex-quantum-entanglement-us"></boxlink>
-
-<boxlink href="https://www.nasa.gov/news-release/liftoff-nasa-tech-science-en-route-to-moon-with-intuitive-machines/"></boxlink>
-
-<boxlink href="https://snyder.substack.com/p/five-failures-in-the-oval-office"></boxlink>
-
-<boxlink href="https://www.nasa.gov/learning-resources/internship-programs/"></boxlink>
-
-<boxlink href="https://variety.com/2025/film/news/rob-lowe-sex-scenes-brave-nicole-kidman-babygirl-1236323922/"></boxlink>
-
-<boxlink href="https://loudwire.com/slayer-2025-canadian-feq-festival/"></boxlink>
+<boxlink href="https://www.nps.gov/articles/000/idkt_shootingstars.htm"></boxlink>
 
 
-
-<!-- boxlink href="../../exercices-css/#end"></boxlink -->
+<boxlink href="../../exercices-css/#end"></boxlink>
